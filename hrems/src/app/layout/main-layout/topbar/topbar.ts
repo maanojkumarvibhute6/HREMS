4091fe7170e7a148119e7513a3833b16b407f60e
Manoj Kumar <maanojkumarvibhute6@gmail.com>
2026-07-05 11:16:14 +0530

    PR - Created the folder structure and installed the required dependencies for ui are primeng, primeng themes, primeng icons and bootstrap latest versions

---
+++ hrems/src/app/layout/main-layout/topbar/topbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [],
+  templateUrl: './topbar.html',
+  styles: ``,
+})
+export class TopbarComponent {}
